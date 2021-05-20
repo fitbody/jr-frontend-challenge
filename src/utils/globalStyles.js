0439a7c9a07bcb43fb5c7a27d94672d14cde7c07
Daniel Goycochea <daniel.goycochea@cumplo.com>
2021-05-20 02:22:02 -0500

    add globalStyle

---
+++ src/utils/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+    font-family: sans-serif;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
